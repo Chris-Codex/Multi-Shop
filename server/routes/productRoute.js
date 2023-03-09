2110af0859047cea1f6a8f9d0a09131c41ce4769
Admin <iheachochristian@yahoo.com>
2023-03-09 23:09:38 +0000

    Api commit

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,3 +1,4 @@
+const { query } = require("express")
 const express = require("express")
 const router = express.Router()
 const productModel = require("../model/products")
@@ -9,16 +10,56 @@ router.post("/", (req, res) => {
         title, price, description, category, image, color, size
     })
 
-
-
     try {
-        const saveProduct = addProduct.save()
-        res.status(200).json({ product: saveProduct })
+        addProduct.save()
+        res.status(200).json({ messsage: "Product Added" })
     } catch (error) {
         res.send({ message: error.message })
     }
+})
 
+router.get("/", async (req, res) => {
 
+    try {
+        const product = await productModel.find()
+        res.send(product)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.get("/:productid", async (req, res) => {
+
+    try {
+        const getProductById = await productModel.findOne(req.body.productid)
+        res.send(getProductById)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.patch("/:productid", async (req, res) => {
+    try {
+        const editProduct = await productModel.updateOne(
+            { id: req.params.productid },
+            {
+                $set: {
+                    title: req.body.title,
+                    price: req.body.price,
+                    description: req.body.description,
+                    category: req.body.category,
+                    image: req.body.image,
+                    color: req.body.color,
+                    size: req.body.size
+                }
+            }
+        )
+
+        res.json(editProduct)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 module.exports = router
+
